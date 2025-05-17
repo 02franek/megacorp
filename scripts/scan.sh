e56c74723d8ca83986d2a441a632a0b8ce396e8c
02franek <127611343+02franek@users.noreply.github.com>
2025-05-17 18:48:51 +0200

    P: Revert "K: credits"
    This reverts commit 1b300e771b0582d7f9e9a5bac6daf0253f38b732.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,6 +1,1 @@
-echo "\n== SCANNING ==" 
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "== SCAN COMPLETE =="
-echo "== OTHER SCAN BLA BLA =="
-echo "== YET ANOTHER SCAN ==" 
-
+# TODO: write the script
